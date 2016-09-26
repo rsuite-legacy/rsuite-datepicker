e4ec1ee4c9c9e5de3264043080ace0849ccad560
theJian <thejianmail@gmail.com>
2016-09-26 16:35:57 +0800

    Update import source

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DatePicker from '../lib';
+import DatePicker from '../src';
 import '../style/Default.less';
 import './app.less';
 
