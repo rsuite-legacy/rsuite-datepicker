40c31b8462f86d1a713d98d98cd9d520392d97e4
simonguo <simon.guo@hypers.com>
2017-10-30 19:00:47 +0800

    Added compression-webpack-plugin for production ENV

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const markdownLoader = require('markdownloader').renderer;
 
 
@@ -31,6 +32,14 @@ const plugins = [
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(new webpack.optimize.UglifyJsPlugin());
+  plugins.push(new webpack.BannerPlugin({ banner: `Last update: ${new Date().toString()}` }));
+  plugins.push(new CompressionPlugin({
+    asset: '[path].gz[query]',
+    algorithm: 'gzip',
+    test: /\.(js|html)$/,
+    threshold: 10240,
+    minRatio: 0.8
+  }));
 }
 
 const common = {
