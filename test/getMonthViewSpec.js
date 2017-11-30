0b37dcaa2cf95bee6a1156b04055d02e7b6482be
simonguo <simon.guo@hypers.com>
2017-11-30 13:20:46 +0800

    Update test case

---
+++ test/getMonthViewSpec.js
@@ -0,0 +1,16 @@
+import moment from 'moment';
+import getMonthView from '../src/utils/getMonthView';
+
+describe('getMonthView', () => {
+
+  it('Sunday is the first day of the week.', () => {
+    const weeks = getMonthView(moment('2017-11-30'));
+    assert.equal(weeks[0].format('YYYY-MM-DD'), '2017-11-26');
+  });
+
+  it('Monday is the first day of the week.', () => {
+    const weeks = getMonthView(moment('2017-11-30'), 'Monday');
+    assert.equal(weeks[0].format('YYYY-MM-DD'), '2017-11-27');
+  });
+
+});
